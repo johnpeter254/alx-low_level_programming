19d761a479f1301bf7f729c7b8a0f9481a1fdf0a
johnpeter254 <johnpeter7773@gmail.com>
2023-10-07 07:03:41 -0700

    numberz

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ *
+ *main - code executes
+ *
+ *
+ * return (0)-success
+ *
+ */
+int main(void)
+{
+int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
