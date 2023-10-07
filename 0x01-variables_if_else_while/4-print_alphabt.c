8f386d871619a2575e94720819fb3d0f2f5acdc7
johnpeter254 <johnpeter7773@gmail.com>
2023-10-07 06:50:26 -0700

    soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+ *
+ * main - start of code execute
+ *
+ *
+ * return (0) -success
+ *
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'q' && alpha != 'e')
+		{
+			putchar(alpha);
+		}
+	}
+	return (0);
+}
