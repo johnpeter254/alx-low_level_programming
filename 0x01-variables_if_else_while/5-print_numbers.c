5c8e22950327c3b4ca2fb217437c7f65f14e6b6b
johnpeter254 <johnpeter7773@gmail.com>
2023-10-07 07:16:24 -0700

    commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,12 +9,14 @@
  */
 int main(void)
 {
-int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        printf("\n");
-    }
+	for (i = 0; i < 10; i+=)
+	{
+		putchar('0' + i`);
+	}
 
-    return 0;
+	putchar('\n');
+
+	return (0);
 }
