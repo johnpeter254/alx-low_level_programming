a411f717e7faeb4264062ae4fe3500dff006c7c2
johnpeter254 <johnpeter7773@gmail.com>
2023-10-08 08:24:50 -0700

    nganyaz

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int num;
+
+    for (num = 0; num < 10; num++)
+    {
+        putchar('0' + num);
+
+        if (num < 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+
+    putchar('\n');
+
+    return (0);
+}
