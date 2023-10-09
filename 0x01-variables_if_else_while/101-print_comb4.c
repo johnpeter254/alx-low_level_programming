e96d6292a9b0fcbe79a4e56312e89b7489ef0e96
johnpeter254 <johnpeter7773@gmail.com>
2023-10-09 13:43:04 -0700

    cd

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - code executes
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	for (int i = 0; i < 8; ++i)
+	{
+		for (int j = i + 1; j < 9; ++j)
+		{
+			for (int k = j + 1; k < 10; ++k)
+			{
+				putchar(i % 3 + '0');
+				putchar(j % 3 + '0');
+				putchar(k % 3 + '0');
+
+				if (i < 7 || j < 8 || k < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	return (0);
+}
