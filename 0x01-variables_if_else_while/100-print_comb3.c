e8266fc3faa4346d1c88a35b75889d43d14f601f
johnpeter254 <johnpeter7773@gmail.com>
2023-10-09 00:21:13 -0700

    100print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,23 +7,19 @@
  */
 int main(void)
 {
-	int digit1, digit2, digit3;
+	int d1, d2;
 
-	for (digit1 = 0; digit1 <= 9; digit1++)
+	for (d1 = 0; d1 <= 8; d1++)
 	{
-		for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
+		for (d2 = d1 + 1; d2 <= 9; d2++)
 		{
-			for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
-			{
-				putchar('0' + digit1);
-				putchar('0' + digit2);
-				putchar('0' + digit3);
+			putchar('0' + d1);
+			putchar('0' + d2);
 
-				if (!(digit1 == 7 && digit2 == 8 && digit3 == 9))
-				{
-					putchar(',');
-					putchar(' ');
-				}
+			if (!(d1 == 8 && d2 == 9))
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
