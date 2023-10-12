509e115e967477f93c36e8b0b96ba7f6403b61ba
johnpeter254 <johnpeter7773@gmail.com>
2023-10-12 11:24:18 -0700

    piano

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,16 +17,20 @@ void times_table(void)
 			{
 				_putchar(result + '0');
 			}
-			else
+			if (result < 10 && i != 0)
 
 			{
 				_putchar(',');
 				_putchar(' ');
-
-				if (result < 10)
-					_putchar(' ');
-
-				_putchar('0' + (result % 10));
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else if (result >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
 			}
 		}
 		_putchar('\n');
