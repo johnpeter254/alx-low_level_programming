0c9a090e3f2c71c30f74dafa20d445c22cd8a2da
johnpeter254 <johnpeter7773@gmail.com>
2023-10-13 05:40:16 -0700

    train

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,6 @@
 #include "main.h"
 
 /**
- * positive_or_negative - prints if the number is positive, negative, or zero
- * @n: integer to be checked
- */
-void positive_or_negative(int n);
-
-/**
  * main - tests function that prints if int is + or -
  * Return: 0
  */
