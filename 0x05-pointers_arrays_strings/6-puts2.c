d24942e2adac2c5561a6a2a188ee8b217494fd4f
johnpeter254 <johnpeter7773@gmail.com>
2023-10-17 10:43:18 -0700

    exp

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string followed buy \n
+ * @str: character to print
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+
+		i += 2;
+	}
+	_putchar('\n');
+}
