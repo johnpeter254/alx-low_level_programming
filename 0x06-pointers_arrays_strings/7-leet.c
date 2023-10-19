35bfd954f08f3348989c584eebdccf3c2f7548c9
johnpeter254 <johnpeter7773@gmail.com>
2023-10-19 06:05:17 -0700

    epiphany

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes string into 1337
+ * @str: string to be encoded
+ *
+ * Rteutn:pointer to encoded string
+ */
+
+char *leet(char *str)
+{
+	char *letters = "aeotlAEOTL";
+	char *leetcode = "430711";
+	int i, j;
+
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; letters[j]; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = leetcode[j / 2];
+				break;
+			}
+		}
+	}
+	return str;
+}
