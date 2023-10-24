e781e41872c69ff35759e905ea445e2e29edf779
johnpeter254 <johnpeter7773@gmail.com>
2023-10-23 23:33:00 -0700

    gate

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character to locate
+ *
+ * Return: NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	if ( c == '\0')
+	{
+		return (s);
+	}
+
+	return (NULL);
+}
