94804c56a733f2007a9cee44d247d6837c5a42e9
johnpeter254 <johnpeter7773@gmail.com>
2023-10-30 06:36:37 -0700

    wanga

---
+++ 0x0A-argc-argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - start of program
+ * @argc: number of command line integers
+ * @argv: number of command line strings
+ *
+ * Return: Always (0)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
