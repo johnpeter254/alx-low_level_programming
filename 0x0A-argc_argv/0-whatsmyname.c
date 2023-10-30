bce9faf20c853fe10e9e452f4f95c614f45994fb
johnpeter254 <johnpeter7773@gmail.com>
2023-10-30 06:50:21 -0700

    betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,8 @@
  * Return: Always (0)
  */
 
-int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+int main(int argc __attribute__((unused)),
+		char *argv[] __attribute__((unused)))
 {
 	printf("%s\n", argv[0]);
 
