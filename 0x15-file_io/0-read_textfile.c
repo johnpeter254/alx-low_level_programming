799e9e94a84d0d4dff76492e13cfc8874ada0a6a
johnpeter254 <johnpeter7773@gmail.com>
2023-11-28 13:36:10 -0800

    change

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads text file and prints to POSIX std output
+ * @filename: name of txt file
+ * @letters: number of letters to read and print
+ *
+ * Return: actual numbers of letters read and printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_descriptor;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
+
+	if (filename == NULL)
+		return (0);
+
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		close(file_descriptor);
+		return (0);
+	}
+
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	free(buffer);
+	close(file_descriptor);
+
+	if (bytes_written != bytes_read)
+		return (0);
+
+	return (bytes_written);
+}
